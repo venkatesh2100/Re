b09c9846776b4287a1bbaf184ce3db67f69e7ee6
venkatesh2100 <119948075+venkatesh2100@users.noreply.github.com>
2024-03-10 14:07:23 +0530

    Hello

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import logo from "./logo.svg";
+import logo from "./logo.svg"
 import reportWebVitals from "./reportWebVitals";
 
 export default function NavBar() {
